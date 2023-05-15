36b0782fdf7ac29ca6e3a7d5563ab6fe61af927c
Brian <bson18@gmail.com>
2023-05-15 18:40:18 -0500

    Add ReviewImages table

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,6 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Spot.belongsTo(models.User, {
+        foreignKey: ownerId
+      })
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId'
+      })
     }
   }
   Spot.init({
