888779a089ff103c12350f2530e7701933a8d0c4
Brian <bson18@gmail.com>
2023-05-16 16:09:34 -0500

    added create spot route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot } = require('../../db/models');
+const { Spot, SpotImage } = require('../../db/models');
 
 const router = express.Router();
 
@@ -31,6 +31,8 @@ router.get('/', async (req, res) => {
             let previewImage = ''
             if (spotImages.length > 0) {
                 previewImage = spotImages[0].url
+            } else {
+                previewImage = 'There is no preview image'
             }
 
             return {
@@ -54,6 +56,51 @@ router.get('/', async (req, res) => {
     )
 
     return res.status(200).json({Spots: spotsAssociations});
+});
+
+const validateSpotCreation = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage('Country is required'),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isDecimal()
+        .withMessage('Latitude is not valid'),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isDecimal()
+        .withMessage('Longitude is not valid'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 50 })
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .isDecimal()
+        .withMessage('Price per day is required'),
+    handleValidationErrors
+];
+
+router.post('/', validateSpotCreation, async (req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+
+    const ownerId = req.user.id;
+
+    const newSpot = await Spot.create({ownerId, address, city, state, country, lat, lng, name, description, price });
+
+    return res.status(201).json(newSpot);
 })
 
 module.exports = router;
