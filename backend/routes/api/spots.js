813eda696cf6c9a0eefe454c1059b5c93281853b
Brian <bson18@gmail.com>
2023-05-17 19:17:46 -0500

    revert change for validator

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -164,11 +164,11 @@ const validateSpotCreation = [
         .withMessage('Country is required'),
     check('lat')
         .exists({ checkFalsy: true })
-        .isLatLong('lat, lng')
+        .isLatLong()
         .withMessage('Latitude is not valid'),
     check('lng')
         .exists({ checkFalsy: true })
-        .isLatLong('lat, lng')
+        .isLatLong()
         .withMessage('Longitude is not valid'),
     check('name')
         .exists({ checkFalsy: true })
