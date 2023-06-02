3969114168b579d763c7c89eb58f50d3c8451030
Brian <bson18@gmail.com>
2023-06-02 18:32:03 -0500

    working through navigation

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -56,6 +56,17 @@ export const signup = (user) => async (dispatch) => {
     return response;
 };
 
+//logout thunk action
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
+
+
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
