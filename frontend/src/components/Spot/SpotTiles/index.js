fc9a0a24b080846698eb0ffe3ca5f38fc10b0cdf
Brian <bson18@gmail.com>
2023-06-05 11:56:36 -0500

    added spot tiles and spots and fixed up validation and dropdown

---
+++ frontend/src/components/Spot/SpotTiles/index.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import SpotTile from "../SpotTile";
+import { csrfFetch } from "../../../store/csrf";
+import './SpotTiles.css'
+
+const SpotTiles = () => {
+    const [spots, setSpots] = useState([])
+
+    useEffect(() => {
+        const query = async () => {
+            const res = await csrfFetch("/api/spots")
+            const data = await res.json()
+
+            if (data && data.Spots) {
+                setSpots(data.Spots)
+            }
+        }
+        query()
+    }, [])
+
+    return (
+        <div className="spot-tiles">
+            {spots.map(({ id, price, city, state, avgRating, previewImage }) => {
+                return (
+                    <SpotTile
+                        key={id}
+                        price={price}
+                        location={`${city}, ${state}`}
+                        avgRating={avgRating}
+                        previewImage={previewImage}
+                    />
+                );
+            })}
+        </div>
+    )
+}
+
+export default SpotTiles;
